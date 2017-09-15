8c206610e2f3317d3e6714781a7049f32015f364
Bob Ershov <raveman@raveman.ru>
2017-09-15 17:35:33 +0300

    Russian localization

---
+++ app/assets/javascripts/tinymce/plugins/uploadfile/langs/ru.js
@@ -0,0 +1,12 @@
+tinyMCE.addI18n("ru", {
+  "Insert a file from your computer": "Вставьте файл с вашего компьютера",
+  "Insert file": "Вставьте файл",
+  "Choose a file": "Выберите файл",
+  "Target": "Цель",
+  "You must choose a file": "Вы должны выбрать файл",
+  "Got a bad response from the server": "Получен ошибочный ответ от сервера",
+  "Did not get a response from the server": "Не получен ответ от сервера",
+  "Insert": "Вставить",
+  "Cancel": "Отмена",
+  "Title": "Название/Текст ссылки",
+});
